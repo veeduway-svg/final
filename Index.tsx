aa44053775a212b52ae02a453a1451c42a84a610
veeduway-svg <veeduway@gmail.com>
2025-11-14 19:49:46 +0100

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import SolutionOverview from "@/components/SolutionOverview";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      <SolutionOverview />
+    </div>
+  );
+};
+
+export default Index;
